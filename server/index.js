28f260ada93aa72027d4b5d8f4bb48f863e87ad8
Karina Barinova <kbarinova11@gmail.com>
2023-01-21 21:47:06 +0000

    Add app server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+const io = require('socket.io')(server, { 
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running...');
+})
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnd');
+    })
+    socket.on('callUser', ({
+        userIdToCall, signalData, from, name
+    }) => {
+        io.to(userIdToCall).emit('callUser', {
+            signal: signalData,
+            from,
+            name
+        })
+    });
+    socket.on('answerCall', ({ to, signal }) => {
+        io.to(to).emit('callAccept', signal)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
