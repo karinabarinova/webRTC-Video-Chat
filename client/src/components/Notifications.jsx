2bfef1ee2977df32fc7149107d155d5020208fbc
Karina Barinova <kbarinova11@gmail.com>
2023-01-22 00:53:18 +0000

    Add Notifications, notify current user when someone is calling

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button } from '@material-ui/core'
+import { SocketContext } from '../SocketContext';
 
 export const Notifications = () => {
+  const { answerCall, call, callAccepted } = useContext(SocketContext);
+
   return (
-    <div>
-        Notifications
-    </div>
+    <>
+        {call.isReceivingCall && !callAccepted && (
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center'
+          }}>
+            <h1>{call.name} is calling: </h1>
+            <Button variant='container' color='primary' onClick={answerCall}>Answer</Button>
+          </div>
+        )}
+    </>
   )
 }
