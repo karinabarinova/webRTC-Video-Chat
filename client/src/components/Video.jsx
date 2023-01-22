6ac14ad34423f06038cc631b73b2a9d2806b6f43
Karina Barinova <kbarinova11@gmail.com>
2023-01-22 00:23:06 +0000

    Add VideoPlayer for two users

---
+++ client/src/components/Video.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Grid, Typography, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    width: '550px',
+    [theme.breakpoints.down('xs')]: {
+      width: '300px',
+    },
+  },
+  paper: {
+    padding: '10px',
+    border: '2px solid black',
+    margin: '10px',
+  },
+}));
+
+export const Video = ({ isMuted = false, curRef, name }) => {
+  const classes = useStyles();
+
+  return (
+      <Paper className={classes.paper}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h5" gutterBottom>{name}</Typography>
+          <video playsInline muted={isMuted} autoPlay className={classes.video} ref={null}/>
+        </Grid>
+      </Paper>
+  )
+}
--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { SocketContext } from '../SocketContext';
+
+import { Video } from './Video';
+
+const useStyles = makeStyles((theme) => ({
+  gridContainer: {
+    justifyContent: 'center',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+}));
 
 export const VideoPlayer = () => {
+  const { name, callAccepted, callEnded, stream, call, currentUserVideoRef, otherUserVideoRef } = useContext(SocketContext)
+  const classes = useStyles();
+
   return (
-    <div>
-        VideoPlayer
-    </div>
+    <Grid container className={classes.gridContainer}>
+      {stream && <Video isMuted curRef={currentUserVideoRef} name={name || 'Name'}/>}
+      { callAccepted && !callEnded && <Video curRef={otherUserVideoRef} name={call.name || 'Name'}/>}
+    </Grid>
   )
 }
